1c5cbdea41a9dac4a564e2f251e84e8043368367
FahadIbnaTuhin <66149819+FahadIbnaTuhin@users.noreply.github.com>
2020-08-12 17:40:00 +0600

    Add files via upload

---
+++ precache-manifest.f6aa90a2945719430f6038c83f8a3569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac28c9079db3f16776b889c4c00e218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aa169d445cffb422f10",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "323c6996e0b643cf6902",
+    "url": "/static/js/2.51a8471e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51a8471e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa169d445cffb422f10",
+    "url": "/static/js/main.59cb603c.chunk.js"
+  },
+  {
+    "revision": "715a2757f4d26df1f99a",
+    "url": "/static/js/runtime-main.168290b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
